9bf4672a695cb16954e9ae05120ddb6a95813b6b
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2024-12-14 12:14:38 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.gmwapp.dudeways"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 30
         versionName = "30.0"
